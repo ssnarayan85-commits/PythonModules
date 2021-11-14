baec1eaba31dfe11cee949d5db192c3195d4aec9
sasirekha <ssnarayanan85@gmail.com>
2021-11-13 22:16:48 -0600

    second commit

--- datatypesAndClasses/json-python.py
+++ datatypesAndClasses/json-python.py
@@ -33,7 +33,7 @@ print(py_countries_list[1])
 
 with open ("C:\\SparkScala\\inputData\\usa.json") as usaFP:
     python_obj = json.load(usaFP)
-/**end of file**/
+## end of file
 
 print(type(python_obj))
 print(python_obj['languages'])
